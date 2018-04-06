173e340d94f77ef5a7760f36fd05e4e2654ad09b
Your Name <you@example.com>
2018-04-06 11:43:30 +0200

    almost relase

---
+++ Source/RimStoryMod.cs
@@ -0,0 +1,42 @@
+﻿using SettingsHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace RimStory
+{
+    class RimStoryMod : Mod
+    {
+        public static RimStorySettings settings;
+
+        public RimStoryMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<RimStorySettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(inRect);
+            listing_Standard.AddLabeledCheckbox("Enable events logger" + ": ", ref settings.enableLogging);
+            listing_Standard.AddLabeledCheckbox("Enable funeral" + ": ", ref settings.enableFunerals);
+            listing_Standard.AddLabeledCheckbox("Enable marriage anniversaries "+ ": ", ref settings.enableMarriageAnniversary);
+            listing_Standard.AddLabeledCheckbox("Enable Memory Day " + ": ", ref settings.enableMemoryDay);
+            listing_Standard.AddLabeledCheckbox("Enable Days of Victory " + ": ", ref settings.enableDaysOfVictory);
+            listing_Standard.AddLabeledCheckbox("Enable individual thoughts " + ": ", ref settings.enableIndividualThoughts);
+            listing_Standard.End();
+            settings.Write();
+        }
+
+        public override string SettingsCategory() => "RimStory settings";
+        
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+        }
+    }
+}
---
+++ Source/RimStorySettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace RimStory 
+{
+    class RimStorySettings : ModSettings
+    {
+        public bool enableAll = true;
+        public bool enableFunerals = true;
+        public bool enableMemoryDay = true;
+        public bool enableMarriageAnniversary = true;
+        public bool enableIndividualThoughts = true;
+        public bool enableDaysOfVictory = true;
+        public bool enableLogging = true;
+     
+
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref this.enableFunerals, "enableFunerals", true);
+            Scribe_Values.Look(ref this.enableMemoryDay, "enableMemoryDay", true);
+            Scribe_Values.Look(ref this.enableMarriageAnniversary, "enableMarriageAnniversary", true);
+            Scribe_Values.Look(ref this.enableIndividualThoughts, "enableIndividualThoughts", true);
+            Scribe_Values.Look(ref this.enableDaysOfVictory, "enableDaysOfVictory", true);
+            Scribe_Values.Look(ref this.enableLogging, "enableLogging", true);
+        }
+    }
+}
